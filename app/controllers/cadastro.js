1c5ecda42908c40aed830aa713dd89414b6094d2
lucaramuniz <lucaaraujor@gmail.com>
2025-10-01 17:05:03 -0300

    Corrigindo e incrementando novas configurações no conteudo do cadastro.js na pasta controller

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,18 +1,27 @@
+const { validationResult, body } = require("express-validator");
 module.exports.cadastro = (application, req, res) => {
-  res.render("cadastro", { validacao: {} });
+  res.render("cadastro", { validacao: {}, dadosForm: {} });
 };
 
-module.exports.cadastrar = (application, req, res) => {
+module.exports.cadastrar = async (application, req, res) => {
   const dadosForm = req.body;
-  req.assert("nome", "campo Nome não pode estar vazio").notEmpty();
-  req.assert("usuario", "campo Usuário não pode estar vazio").notEmpty();
-  req.assert("senha", "campo Senha não pode estar vazio").notEmpty();
-  req.assert("casa", "campo Casa não pode estar vazio").notEmpty();
-  const erros = req.validationErros();
-  if (erros) {
-    res.send("cadastro", { validacao: erros });
+
+  // Validação manual
+  await body("nome", "campo Nome não pode estar vazio").notEmpty().run(req);
+  await body("usuario", "campo Usuário não pode estar vazio")
+    .notEmpty()
+    .run(req);
+  await body("senha", "campo Senha não pode estar vazio").notEmpty().run(req);
+  await body("casa", "campo Casa não pode estar vazio").notEmpty().run(req);
+
+  const erros = validationResult(req);
+
+  if (!erros.isEmpty()) {
+    res.render("cadastro", { validacao: erros.array(), dadosForm: dadosForm });
     return;
-  } else {
-    res.send("Podemos cadastrar");
   }
+  const connection = await application.config.dbConnection;
+  const UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+  UsuariosDAO.inserirUsuario(dadosForm);
+  res.send("Podemos cadastrar");
 };
